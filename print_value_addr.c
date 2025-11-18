7d8058ca0391010c7c5eac60c5a64322ff3da31b
rephaelriahi-hash <rephaelriahi@gmail.com>
2025-11-18 15:53:42 +0200

    Add files via upload

---
+++ print_value_addr.c
@@ -0,0 +1,52 @@
+/*------------------------------------------------------
+* Filename: print_value_addr.c
+* Description: The program gets an array of integers and an array of characters and prints 
+* the values and addresses of each
+* Author: Rephael Riahi
+-------------------------------------------------------*/
+
+#include <stdio.h>
+
+#define ARRAY_SIZE 8
+
+int main() {
+    float input_int_check = 0;
+    char input_ch_check = '\0';
+    int arr_of_int [ARRAY_SIZE] = {0};
+    char arr_of_ch [ARRAY_SIZE + 1] = {'\0'};
+    
+    printf("Enter the array of integers: \n");
+    for(int i = 0; i < ARRAY_SIZE; i++){
+        if(scanf("%f", &input_int_check) != 1 || input_int_check - (int)input_int_check != 0){
+            printf("Wrong input!");
+            return 0;
+        }
+        *(arr_of_int + i) = (int)input_int_check;
+    }
+
+   printf("Enter the array of charaacters: \n");
+    for(int i = 0; i < ARRAY_SIZE; i++){
+        scanf(" %c", &input_ch_check);
+        if(input_ch_check >= 'A' && input_ch_check <= 'Z'||input_ch_check >= 'a' && input_ch_check <= 'z'){
+        *(arr_of_ch + i) = input_ch_check;
+        }else{
+            printf("Wrong input!");
+            return 0;
+        }
+    }
+
+    printf("Array of integers printed with * \n");
+    for(int i = 0; i < ARRAY_SIZE; i++){
+        printf("value: %d address: %X\n",*(arr_of_int + i),(arr_of_int + i));
+    }
+
+    printf("\nArray of characters printed with [] \n");
+    for(int i = 0; i < ARRAY_SIZE; i++){
+        printf("value: %c address: %X\n",arr_of_ch[i],&arr_of_ch[i]);
+    }
+    
+    char x;
+    scanf("%c",&x);
+    printf("%d",(int)x);
+    return 0;
+}
